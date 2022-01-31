d773294d78951851ea1d8ebcbafe34a217733c6a
azharmo22350 <98511006+azharmo22350@users.noreply.github.com>
2022-01-31 18:53:10 +0530

    Update total.R

--- total.R
+++ total.R
@@ -6,6 +6,8 @@ library(ggplot2)
 library(fpp2)
 library(writexl)
 
+klkmmkkjj
+
 datagen<- read.csv("C:/Users/azhar/Desktop/forecast/generationForecast.csv")
 genarationforecast<-ts(datagen$Point.Forecast,frequency=2*48)
 
